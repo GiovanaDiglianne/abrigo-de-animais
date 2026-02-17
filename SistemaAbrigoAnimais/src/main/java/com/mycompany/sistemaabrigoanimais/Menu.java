02541cac235c18b9942075298af6a9c09147ea23
Giovana <giodiglianne@gmail.com>
2026-02-17 15:33:33 -0400

    Atualizações no menu

--- SistemaAbrigoAnimais/src/main/java/com/mycompany/sistemaabrigoanimais/Menu.java
+++ SistemaAbrigoAnimais/src/main/java/com/mycompany/sistemaabrigoanimais/Menu.java
@@ -24,15 +24,15 @@ public class Menu {
     
     public void mostrarOpcoes(){
         System.out.println("\n ABRIGO DE ANIMAIS - Giovana Diglianne");
-        System.out.println("1 - Cadastrar Animal");
+        System.out.println("1 - Registrar Resgate");
         System.out.println("2 - Cadastrar Veterinário");
         System.out.println("3 - Cadastrar Secretária");
-        System.out.println("4 - Registrar Resgate");
-        System.out.println("5 - Registrar Adoção");
-        System.out.println("6 - Agendar Consulta Veterinária");
-        System.out.println("7 - Listar Consultas");
+        System.out.println("4 - Registrar Adoção");
+        System.out.println("5 - Agendar Consulta Veterinária");
+        System.out.println("6 - Listar Consultas");
+        System.out.println("7 - Listar Resgates");
+        System.out.println("8 - Listar Adoções");
         System.out.println("99 - Sair");
-        // colocar um listar consultas
     }
     
     public void executar(){
@@ -54,8 +54,8 @@ public class Menu {
     private void checarOpcao(){
         switch(opcao){
             case 1:
-                System.out.println("Cadastrando Animal");
-                cadastrarAnimal();
+                System.out.println("Registrando Resgate");
+                registrarResgate();
                 break;
             case 2:
                 System.out.println("Cadastrando Veterinário");
@@ -66,23 +66,27 @@ public class Menu {
                 cadastrarSecretaria();
                 break;
             case 4:
-                System.out.println("Registrando Resgate");
-                registrarResgate();
-                break;
-            case 5:
                 System.out.println("Resgistrando Adoção");
                 listarAnimais();
                 registrarAdocao();
                 break;
-            case 6:
+            case 5:
                 System.out.println("Agendando Consulta");
                 listarAnimais();
                 listarFuncionarios();
                 agendarConsulta();
                 break;
-            case 7:
+            case 6:
                 System.out.println("Listando Consultas");
                 listarConsultas();
+                break;
+            case 7:
+                System.out.println("Listando Resgates");
+                listarResgates();
+                break;
+            case 8:
+                System.out.println("Listando Adoções");
+                listarAdocoes();
                 break;
             case 99:
                 System.out.println("Saindo do Sistema...");
@@ -114,6 +118,20 @@ public class Menu {
         }
     }
     
+    private void listarResgates(){
+        for (int i = 0; i < resgates.size(); i++) {
+            Resgate r = resgates.get(i);
+            System.out.println(i + " " + r.toString());            
+        }
+    }
+    
+    private void listarAdocoes(){
+        for (int i = 0; i < adocoes.size(); i++) {
+            Adocao a = adocoes.get(i);
+            System.out.println(i + " " + a.toString());            
+        }
+    }
+    
     private void lerNome(Funcionario funcionario){
         while(true) {
             System.out.print("Digite o nome do funcionário: ");
@@ -136,6 +154,18 @@ public class Menu {
                 break;
             } catch(RuntimeException erro) {
                 System.out.println(erro.getMessage());
+            }
+        }
+    }
+    private void lerCpf(Funcionario funcionario) {
+        while (true) {
+            System.out.print("Digite o CPF (apenas números): ");
+            String cpf = teclado.nextLine();
+            try {
+                funcionario.setCpf(cpf); 
+                break; 
+            } catch (RuntimeException erro) {
+                System.out.println("Erro: " + erro.getMessage());
             }
         }
     }
@@ -171,6 +201,111 @@ public class Menu {
         idx = getInt("Selecione o índice da Secretaria: ");
         c.setSecretaria((Secretaria) funcionarios.get(idx));
         consultas.add(c);
+        System.out.println("Agendamento realizado com sucesso!");
     }
     
+    private void cadastrarVeterinario(){
+        Veterinario funcionario = new Veterinario();
+        teclado.nextLine();
+        lerNome(funcionario);
+        lerCpf(funcionario);
+        
+        System.out.print("Digite o CRMV: ");
+        String crmv = teclado.nextLine();
+        funcionario.setCrmv(crmv);
+        
+        System.out.print("Digite a Especialidade: ");
+        String especialidade = teclado.nextLine();
+        funcionario.setEspecialidade(especialidade);
+        
+        System.out.print("Digite o salário: ");
+        float salario = teclado.nextFloat();
+        funcionario.setSalario(salario);
+        teclado.nextLine();
+        
+        funcionarios.add(funcionario);
+        System.out.println("Cadastramento realizado com sucesso!");
+    }
+    
+    private void cadastrarSecretaria(){
+        Secretaria funcionario = new Secretaria();
+        teclado.nextLine();
+        lerNome(funcionario);
+        lerCpf(funcionario);
+        
+        System.out.print("Digite o email: ");
+        String email = teclado.nextLine();
+        funcionario.setEmail(email);
+        
+        System.out.print("Digite o telefone: ");
+        String telefone = teclado.nextLine();
+        funcionario.setTelefone(telefone);
+        
+        System.out.print("Digite a data de nascimento: ");
+        String data = teclado.nextLine();
+        funcionario.setDataNascimento(data);
+        
+        System.out.print("Digite o salário: ");
+        float salario = teclado.nextFloat();
+        funcionario.setSalario(salario);
+        teclado.nextLine();
+        
+        funcionarios.add(funcionario);
+        System.out.println("Cadastramento realizado com sucesso!");
+    }
+    
+    private void registrarResgate(){
+        Animal animal = new Animal();
+        teclado.nextLine();
+        lerNome(animal);
+        
+        System.out.print("Digite a especie: ");
+        String especie = teclado.nextLine();
+        animal.setEspecie(especie);
+        
+        System.out.print("Digite a raça: ");
+        String raca = teclado.nextLine();
+        animal.setRaca(raca);
+
+        int idade = getInt("Digite a idade em meses: ");
+        animal.setIdade(idade);
+        teclado.nextLine();
+        
+        System.out.print("Digite o status: ");
+        String status = teclado.nextLine();
+        animal.setStatus(status);
+        
+        animais.add(animal);
+        
+        Resgate resgate = new Resgate();
+        System.out.print("Data do Resgate: ");
+        resgate.setDataResgate(teclado.nextLine());
+
+        resgate.setAnimal(animal); 
+        resgates.add(resgate); 
+        System.out.println("Registramento realizado com sucesso!");
+    }
+    
+    private void registrarAdocao(){
+        listarAnimais(); 
+        int idx = getInt("Selecione o índice do animal que será adotado: ");
+        Animal animal = animais.get(idx);
+        
+        Adocao adocao = new Adocao();
+        teclado.nextLine();
+        
+        System.out.print("Digite o nome do adotante: ");
+        String adotante = teclado.nextLine();
+        adocao.setAdotante(adotante);
+        
+        System.out.print("Digite a data da adoção: ");
+        String dataAdocao = teclado.nextLine();
+        adocao.setDataAdocao(dataAdocao);
+        
+        adocao.setAnimal(animal);
+        animal.setStatus("Adotado");
+        
+        adocoes.add(adocao);
+        System.out.println("Registramento realizado com sucesso!");
+    }
 }
